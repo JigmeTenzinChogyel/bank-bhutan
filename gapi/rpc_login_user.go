62b9a23f202314c85fc0a6663e4087df4649ba20
JigmeTenzinChogyel <jigmetenzinchogyel@gmail.com>
2025-06-10 14:17:47 +0600

    grpc handlers

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,38 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/JigmeTenzinChogyel/bank-bhutan/pb"
+	"github.com/JigmeTenzinChogyel/bank-bhutan/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found %s: ", err)
+		}
+		return nil, status.Errorf(codes.Internal, "failed to get user %s: ", err)
+	}
+
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "validation failed %s: ", err)
+	}
+
+	accessToken, err := server.tokenMaker.CreateToken(user.Username, server.config.AccessTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create token %s: ", err)
+	}
+
+	rsp := &pb.LoginUserResponse{
+		AccessToken: accessToken,
+		User:        convertUser(user),
+	}
+
+	return rsp, nil
+}
